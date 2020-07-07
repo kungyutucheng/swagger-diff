2fed521718a65f6a38b6142179a08f570e9518ce
wengyongcheng <wengyongcheng@rongshantech.com>
2020-07-07 18:53:23 +0800

    新增对比功能

--- src/main/java/com/kungyu/task/DiffTask.java
+++ src/main/java/com/kungyu/task/DiffTask.java
@@ -13,6 +13,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -36,6 +37,8 @@ public class DiffTask implements Callable<List<DiffResult>> {
     private List<DiffResult> diffResultList = new ArrayList<>();
 
     private String currentDiffPath = null;
+
+    private String currentFieldName = null;
 
     public DiffTask(@NotNull String newUrl, @NotNull String origUrl) {
         this.newUrl = newUrl;
@@ -201,9 +204,71 @@ public class DiffTask implements Callable<List<DiffResult>> {
 
     private void diffHeader(Header newHeader, Header origHeader) {
         diffDesc(newHeader.getDescription(),origHeader.getDescription(),ChangeType.OUTPUT_HEADER_DESC_MODIFY);
+        diffString(newHeader.getType(), origHeader.getType(), ChangeType.OUTPUT_HEADER_TYPE_MODIFY);
+        diffString(newHeader.getFormat(), origHeader.getFormat(), ChangeType.OUTPUT_HEADER_FORMAT_MODIFY);
+
+        diffItems(newHeader.getItems(), origHeader.getItems());
+
+        diffString(newHeader.getCollectionFormat(), origHeader.getCollectionFormat(), ChangeType.OUTPUT_HEADER_COLLECTION_FORMAT_MODIFY);
+        diffString(newHeader.getDefaultValue(), origHeader.getDefaultValue(), ChangeType.OUTPUT_HEADER_DEFAULT_VALUE_MODIFY);
+        diffBigDecimal(newHeader.getMaximum(), origHeader.getMaximum(), ChangeType.OUTPUT_HEADER_MAXIMUM_MODIFY);
+        diffBoolean(newHeader.getExclusiveMaximum(),origHeader.getExclusiveMaximum(),ChangeType.OUTPUT_HEADER_EXCLUSIVE_MAXIMUM_MODIFY);
+        diffBigDecimal(newHeader.getMinimum(), origHeader.getMinimum(), ChangeType.OUTPUT_HEADER_MINIMUM_MODIFY);
+        diffBoolean(newHeader.getExclusiveMinimum(), origHeader.getExclusiveMinimum(), ChangeType.OUTPUT_HEADER_EXCLUSIVE_MINIMUM_MODIFY);
+        diffInteger(newHeader.getMaxLength(), origHeader.getMaxLength(), ChangeType.OUTPUT_HEADER_MAXLENGTH_MODIFY);
+        diffInteger(newHeader.getMinLength(), origHeader.getMinLength(), ChangeType.OUTPUT_HEADER_MINLENGTH_MODIFY);
+        diffString(newHeader.getPattern(), origHeader.getPattern(), ChangeType.OUTPUT_HEADER_PATTERN_MODIFY);
+        diffInteger(newHeader.getMaxItems(), origHeader.getMaxItems(), ChangeType.OUTPUT_HEADER_MAX_ITEMS_MODIFY);
+        diffInteger(newHeader.getMinItems(), origHeader.getMinItems(), ChangeType.OUTPUT_HEADER_MIN_ITEMS_MODIFY);
+        diffBoolean(newHeader.getUniqueItems(), origHeader.getUniqueItems(), ChangeType.OUTPUT_HEADER_UNIQUE_ITEMS_MODIFY);
+        diffStringList(newHeader.getEnumValues(), origHeader.getEnumValues(), ChangeType.OUTPUT_HEADER_ENUM_VALUES_MODIFY);
+        diffBigDecimal(newHeader.getMultipleOf(),origHeader.getMultipleOf(),ChangeType.OUTPUT_HEADER_MULTIPLE_OF_MODIFY);
+    }
+
+    private void diffItems(Item newItem, Item origItem) {
+
+        diffString(newItem.getType(), origItem.getType(), ChangeType.OUTPUT_HEADER_ITEM_TYPE_MODIFY);
+        diffString(newItem.getFormat(), origItem.getFormat(), ChangeType.OUTPUT_HEADER_ITEM_FORMAT_MODIFY);
+
+        diffString(newItem.getCollectionFormat(), origItem.getCollectionFormat(), ChangeType.OUTPUT_HEADER_ITEM_COLLECTION_FORMAT_MODIFY);
+        diffString(newItem.getDefaultValue(), origItem.getDefaultValue(), ChangeType.OUTPUT_HEADER_ITEM_DEFAULT_VALUE_MODIFY);
+        diffBigDecimal(newItem.getMaximum(), origItem.getMaximum(), ChangeType.OUTPUT_HEADER_ITEM_MAXIMUM_MODIFY);
+        diffBoolean(newItem.getExclusiveMaximum(),origItem.getExclusiveMaximum(),ChangeType.OUTPUT_HEADER_ITEM_EXCLUSIVE_MAXIMUM_MODIFY);
+        diffBigDecimal(newItem.getMinimum(), origItem.getMinimum(), ChangeType.OUTPUT_HEADER_ITEM_MINIMUM_MODIFY);
+        diffBoolean(newItem.getExclusiveMinimum(), origItem.getExclusiveMinimum(), ChangeType.OUTPUT_HEADER_ITEM_EXCLUSIVE_MINIMUM_MODIFY);
+        diffInteger(newItem.getMaxLength(), origItem.getMaxLength(), ChangeType.OUTPUT_HEADER_ITEM_MAXLENGTH_MODIFY);
+        diffInteger(newItem.getMinLength(), origItem.getMinLength(), ChangeType.OUTPUT_HEADER_ITEM_MINLENGTH_MODIFY);
+        diffString(newItem.getPattern(), origItem.getPattern(), ChangeType.OUTPUT_HEADER_ITEM_PATTERN_MODIFY);
+        diffInteger(newItem.getMaxItems(), origItem.getMaxItems(), ChangeType.OUTPUT_HEADER_ITEM_MAX_ITEMS_MODIFY);
+        diffInteger(newItem.getMinItems(), origItem.getMinItems(), ChangeType.OUTPUT_HEADER_ITEM_MIN_ITEMS_MODIFY);
+        diffBoolean(newItem.getUniqueItems(), origItem.getUniqueItems(), ChangeType.OUTPUT_HEADER_ITEM_UNIQUE_ITEMS_MODIFY);
+        diffStringList(newItem.getEnumValues(), origItem.getEnumValues(), ChangeType.OUTPUT_HEADER_ITEM_ENUM_VALUES_MODIFY);
+        diffBigDecimal(newItem.getMultipleOf(),origItem.getMultipleOf(),ChangeType.OUTPUT_HEADER_ITEM_MULTIPLE_OF_MODIFY);
     }
 
     private void diffSchema(Schema newSchema, Schema origSchema, SchemaType schemaType) {
+        // 通过ref判断是否为引用，如果是，从definition中获取到对应到数据，接着对Definition中的Schema进行递归对比
+        if (StringUtils.isNotBlank(newSchema.getRef()) && StringUtils.isNotBlank(origSchema.getRef())) {
+            Schema newRefSchema = null;
+            Schema origRefSchema = null;
+            for (Definition definition : newDefinitionList) {
+                if (StringUtils.equals(definition.getName(),newSchema.getRef())) {
+                    newRefSchema = definition.getSchema();
+                    break;
+                }
+            }
+            for (Definition definition : origDefinitionList) {
+                if (StringUtils.equals(definition.getName(),origSchema.getRef())) {
+                    origRefSchema = definition.getSchema();
+                    break;
+                }
+            }
+            this.diffString(newSchema.getRef(),origRefSchema.getRef(),ChangeType.OUTPUT_PARAMETER_NAME_MODIFY);
+            this.diffSchema(newRefSchema,origRefSchema,schemaType);
+        }
+
+        this.diffString(newSchema.getFormat(), origSchema.getFormat(), ChangeType.OUTPUT_HEADER_MULTIPLE_OF_MODIFY);
+
     }
 
     private void diffParameter(List<Parameter> newParameters, List<Parameter> origParameters) {
@@ -271,6 +336,38 @@ public class DiffTask implements Callable<List<DiffResult>> {
         }
     }
 
+    private void diffBigDecimal(BigDecimal newValue, BigDecimal origValue, ChangeType changeType) {
+        if (newValue == null && origValue != null) {
+            this.buildDiffResult(currentDiffPath, origValue.toString(), null, null, changeType, null);
+        } else if (newValue != null && origValue == null) {
+            this.buildDiffResult(currentDiffPath, null, newValue.toString(), null, changeType, null);
+        } else if (newValue != null){
+            if (newValue.compareTo(origValue) != 0) {
+                this.buildDiffResult(currentDiffPath, origValue.toString(), newValue.toString(), null, changeType, null);
+            }
+        }
+    }
+
+    private void diffBoolean(Boolean newValue, Boolean origValue, ChangeType changeType) {
+        if (newValue == null && origValue != null) {
+            this.buildDiffResult(origValue + "", null, changeType);
+        } else if (newValue != null && origValue == null) {
+            this.buildDiffResult(null, newValue + "", changeType);
+        } else if (newValue != null) {
+            this.buildDiffResult(origValue + "", newValue + "", changeType);
+        }
+    }
+
+    private void diffInteger(Integer newValue, Integer origValue, ChangeType changeType) {
+        if (newValue == null && origValue != null) {
+            this.buildDiffResult(origValue + "", null, changeType);
+        } else if(newValue != null && origValue == null) {
+            this.buildDiffResult(null,newValue + "",changeType);
+        } else if (newValue != null) {
+            this.buildDiffResult(origValue + "", newValue + "",changeType);
+        }
+    }
+
     private void buildDiffResult(String url, String origValue, String newValue, String fieldName, ChangeType changeType, String desc) {
         DiffResult diffResult = new DiffResult();
         diffResult.setUrl(url);
@@ -282,5 +379,9 @@ public class DiffTask implements Callable<List<DiffResult>> {
         diffResultList.add(diffResult);
     }
 
+
+    private void buildDiffResult(String origValue, String newValue, ChangeType changeType) {
+        this.buildDiffResult(currentDiffPath,origValue,newValue,null,changeType,null);
+    }
 
 }
